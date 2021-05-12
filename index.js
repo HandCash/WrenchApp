6f482d4971877102b0e1e24b2d2e6dc77b03c4b3
genericcpu <brandongcryderman@gmail.com>
2021-05-11 23:16:03 -0300

    refactor

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const rootDir = require('./util/path');
   
   //connect to mongodb
   mongoose
-    .connect(process.env.database, { useNewUrlParser: true })
+    .connect(process.env.database)
     .then(() => console.log("Connected to MongoDB..."))
     .catch(err => console.error("Could not connect to MongoDB..."));
 
